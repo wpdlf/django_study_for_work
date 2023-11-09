fda3fec5120f05c17ad2ebd6e74763f5743ec760
wpdlf <ckjjw2953@naver.com>
2023-11-09 09:06:59 +0000

    admin.py 오타 수정

--- blog/admin.py
+++ blog/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from .models import Post, Category, Tag
-from markdownx.admin import MarkdownModelAdmin
+from markdownx.admin import MarkdownxModelAdmin
 
 # Register your models here.
-admin.site.register(Post, MarkdownModelAdmin)
+admin.site.register(Post, MarkdownxModelAdmin)
 
 class CategoryAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name', )}  # Category 모델의 name 필드에 값이 입력됐을 때 자동으로 slug가 만들어진다.
