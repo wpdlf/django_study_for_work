b5f3324072302f295187d57ac4544504ec229b8d
wpdlf <ckjjw2953@naver.com>
2023-11-09 08:42:52 +0000

    django-crispy-forms 적용하기

--- blog_proj/settings.py
+++ blog_proj/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+
+    'crispy_forms',
+    'crispy_bootstrap4',
     
     'blog',
     'single_pages',
@@ -126,6 +129,8 @@ STATIC_URL = '/static/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, '_media')
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
