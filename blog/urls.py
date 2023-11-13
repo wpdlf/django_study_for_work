9fa55c48755b60a235568e83c8299852a1d08af7
wpdlf <ckjjw2953@naver.com>
2023-11-13 06:24:31 +0000

    댓글 수정 기능 추가하기

--- blog/urls.py
+++ blog/urls.py
@@ -9,6 +9,7 @@ urlpatterns = [
     path('create_post/', views.PostCreate.as_view()),
     path('update_post/<int:pk>/', views.PostUpdate.as_view()),
     path('<int:pk>/new_comment/', views.new_comment),
+    path('update_comment/<int:pk>/', views.CommentUpdate.as_view()),
 
     # path('', views.index),
     # path('<int:pk>/', views.single_post_page),
--- blog/views.py
+++ blog/views.py
@@ -3,7 +3,7 @@ from django.views.generic import ListView, DetailView, CreateView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 # LoginRequiredMixin : 장고에서 제공하는 클래스로 view.py에서 임포트하고 사용한다. 매개변수로 추가하면 로그인했을 때만 페이지가 보이게 된다.
 from django.shortcuts import get_object_or_404
-from .models import Post, Category, Tag
+from .models import Post, Category, Tag, Comment
 from .forms import CommentForm
 from django.core.exceptions import PermissionDenied
 from django.utils.text import slugify
@@ -143,6 +143,18 @@ class PostUpdate(LoginRequiredMixin, UpdateView):
                 self.object.tags.add(tag)
 
         return response
+
+
+class CommentUpdate(LoginRequiredMixin, UpdateView):
+    model = Comment
+    form_class = CommentForm
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated and request.user == self.get_object().author:
+            return super(CommentUpdate, self).dispatch(request, *args, **kwargs)
+
+        else:
+            raise PermissionDenied
 
 
 def category_page(request, slug):
